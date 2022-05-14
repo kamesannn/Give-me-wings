8bcc60909bdfcfa61637725fdd0736cbdb46a16c
ftong105 <tfgylsy@gmail.com>
2022-05-14 20:34:14 +1200

    background

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
 
 
+class Background(Widget):
+    cloud_texture = ObjectProperty(None)
 
-class MyGridLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGridLayout, self).__init__(**kwargs)
+    def __int__(self, **kwargs):
+        super().__init__(**kwargs)
 
-        self.cols = 1
+        # Create Textures
+        self.cloud_texture = Image(source="1838449.png").texture
+        self.cloud_texture.wrap = 'repeat'
+        self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
 
-        self.add_widget(Label(text="Name: ", font_size=32))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
-
-        self.enter = Button(text="Enter", font_size=32)
-
-        self.enter.bind(on_press=self.press)
-        self.add_widget(self.enter)
-
-    def press(self, instance):
-        name = self.name.text
-        print(f"Hello {name}!!")
+    pass
 
 
-class MyApp(App):
-    def build(self):
-        self.title = "window"
-        return MyGridLayout()
+class GiveMeWings(App):
+    pass
 
 
-if __name__ == '__main__':
-    MyApp().run()
+GiveMeWings().run()
