e9afd1b52ad4e34fce0a6a948c83109266fd8a3a
kamesannn <84747125+kamesannn@users.noreply.github.com>
2022-05-17 11:28:05 +1200

    font family added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import kivy
+
 from kivy.app import App
-from kivy.uix.widget import Widget
+# from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.label import Label
@@ -8,12 +8,14 @@ from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 Builder.load_file('main.kv')
 
-class FloatLayout(FloatLayout):
-    pass
-class MainLayout(Widget):
+#class FloatLayout(FloatLayout):
+#    pass
+class MainLayout(BoxLayout):
     pass
 
 class MyApp(App):
