81c47beafcc7c505316a1588602690d67fca08a8
ftong105 <tfgylsy@gmail.com>
2022-05-16 15:59:37 +1200

    cloud and floor scroll

--- main.py
+++ main.py
@@ -8,28 +8,42 @@ from kivy.clock import Clock
 
 class Background(Widget):
     cloud_texture = ObjectProperty(None)
+    floor_texture = ObjectProperty(None)
 
-    def __int__(self, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # Create Textures
+        # Create textures
         self.cloud_texture = Image(source="1838449.png").texture
         self.cloud_texture.wrap = 'repeat'
         self.cloud_texture.uvsize = (Window.width / self.cloud_texture.width, -1)
 
+        self.floor_texture = Image(source="NicePng_minecraft-dirt-block-png_2255256.png").texture
+        self.floor_texture.wrap = 'repeat'
+        self.floor_texture.uvsize = (Window.width / self.floor_texture.width, -1)
+
+    def on_size(self, *args):
+        self.cloud_texture.uvsize = (self.width / self.cloud_texture.width, -1)
+        self.floor_texture.uvsize = (self.width / self.floor_texture.width, -1)
+
     def scroll_textures(self, time_passed):
         # Update the uvpos of the texture
+        self.cloud_texture.uvpos = ( (self.cloud_texture.uvpos[0] + time_passed)%Window.width, self.cloud_texture.uvpos[1])
+        self.floor_texture.uvpos = ( (self.floor_texture.uvpos[0] + time_passed)%Window.width, self.floor_texture.uvpos[1])
 
         # Redraw the texture
-        print("scroll")
+        texture = self.property('cloud_texture')
+        texture.dispatch(self)
 
-    pass
+        texture = self.property('floor_texture')
+        texture.dispatch(self)
 
 
 class GiveMeWings(App):
     def on_start(self):
-        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/2.)
+        Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/60.)
     pass
+
 
 
 GiveMeWings().run()
