2177baea1237ffa6532ca6a6ba7564866dec0ee4
kamesannn <84747125+kamesannn@users.noreply.github.com>
2022-05-23 22:00:35 +1200

    bgm

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.audio import SoundLoader
 from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+
 
 # Window.size = (1200, 800)
 
@@ -26,7 +28,11 @@ kv = Builder.load_file('main.kv')
 
 
 class GiveMeWingsApp(App):
+
     def build(self):
+        self.music = SoundLoader.load('03-Meydan-Tired-of-life.mp3')
+        self.music.play()
+
         return kv
 
 
